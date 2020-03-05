c1930223cf6706d00630e5902c8592e43693d85d
mireknosek <mireknosek@gmail.com>
2020-03-05 06:29:04 +0100

    tab[][]

--- src/nauka/Tablice.java
+++ src/nauka/Tablice.java
@@ -4,5 +4,36 @@ public class Tablice {
 
     public static void main(String[] args) {
 
+        int[][] tab = new int[4][];
+        tab[0] = new int[4];
+        tab[1] = new int[3];
+        tab[2] = new int[2];
+        tab[3] = new int[1];
+        int licznik = 1;
+        int i = 0;
+        int j = 0;
+        while (i < tab.length) {
+            System.out.println("przebieg: " + i);
+            while (j < tab[i].length) {
+                System.out.println("wpis: " + j);
+                tab[i][j] = licznik;
+                licznik++;
+                j++;
+            }
+            i++;
+        }
+        System.out.println(licznik);
+        System.out.println("==============");
+        i = 0;
+        j = 0;
+        while (i < tab.length) {
+            while (j < tab[i].length) {
+                System.out.println(tab[i][j]);
+                j++;
+            }
+            i++;
+        }
     }
 }
+
+
